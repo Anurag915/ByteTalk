6c023b515bd1e6b65793a6503fbd42c42654c833
ANURAG PRAJAPATI <anuragprajapati02005@gmail.com>
2025-08-10 12:39:59 +0530

    add production environment

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ io.on("connection", (socket) => {
 
 //Middleware setup
 app.use(express.json({ limit: "4mb" }));
-app.use(cors());
+app.use(cors({ origin: "*"}));
 
 app.use("/api/status", (req, res) => {
   res.send("server is running");

2234dab9b9eb29c5f0bef450b1c29a7c1e784d13
ANURAG PRAJAPATI <anuragprajapati02005@gmail.com>
2026-03-08 21:43:14 +0530

    Fix Render port binding

--- server/server.js
+++ server/server.js
@@ -149,13 +149,11 @@ app.use("/api/messages", messageRouter);
 
 await connectDB();
 
-if (process.env.NODE_ENV != "production") {
-  const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-  server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 //export server function for vercel
 export default server;
